fd0ae45eb6f8ebcc40bcf871a3c4815711090c07
Naveen Arockia Raj <naveen.arockiaraj@view.com>
2024-07-08 11:21:18 +0530

    Basic python variables

---
+++ app.py
@@ -0,0 +1,10 @@
+meaning = 1
+print('')
+
+# if meaning > 10: 
+#     print('Right on!')
+# else:
+#     print('not today')
+
+# trnary operator
+print('Right on!') if meaning > 10 else print('Not today')
